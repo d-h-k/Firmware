bb4ab2bbac321338b955bb2bebd74ebba2bc6dda
dongdongKing <ultraraptor@naver.com>
2023-03-24 02:03:41 +0900

    하드웨어 레이어 추가

---
+++ stm32f411-firmware/Src/hw/hw.h
@@ -0,0 +1,6 @@
+#ifndef SRC_HW_HW_H_
+#define SRC_HW_HW_H_
+
+void hwInit(void);
+
+#endif
---
+++ stm32f411-firmware/Src/hw/hw_def.h
@@ -0,0 +1,7 @@
+#ifndef SRC_HW_HW_DEF_H_
+#define SRC_HW_HW_DEF_H_
+
+#include "def.h"
+#include "bsp.h"
+
+#endif

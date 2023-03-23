8584d1f0bfc812829f9e6a04ac372b01ef666ade
dongdongKing <ultraraptor@naver.com>
2023-03-24 01:58:27 +0900

    common module

--- stm32f411-firmware/Src/common/def.h
+++ stm32f411-firmware/Src/common/def.h
@@ -1,1 +1,9 @@
-typedef unsigned int uint32_t;
+#ifndef SRC_COMMON_DEF_H
+#define SRC_COMMON_DEF_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+//typedef unsigned int uint32_t;
+
+#endif
